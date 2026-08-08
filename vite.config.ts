06417fcd6b77e6b95acae108f9ec4f978cea579e
oguzhanoruchan <oguzhan.oruchan@gmail.com>
2026-08-08 13:41:35 +0000

    fix: correct production asset base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/nislen-cafe-menu/',
+  base: '/',
   build: {
     sourcemap: true,
     chunkSizeWarningLimit: 900,
